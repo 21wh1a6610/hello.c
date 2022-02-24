844f3a7d21f1b6ae977efb06000521a734851912
21wh1a6610 <96616282+21wh1a6610@users.noreply.github.com>
2022-02-24 21:45:09 +0530

    Add files via upload

---
+++ switch statements.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int choice;
+	scanf("%d",&choice);
+	switch(choice)
+{
+	case 1:
+	printf("pizza,Rs 239");
+	break;
+	case 2:
+	printf("burger,Rs 129");
+	break;
+	case 3:
+	printf("french fries,Rs 179");
+	break;
+	case 4:
+	printf("pasta,Rs 169");
+	break;
+	case 5:
+	printf(" sandwich,Rs 149");
+		
+	}	
+	
+}
